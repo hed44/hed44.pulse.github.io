b508ab21cd17cefff82068818ddcd89a6cf428ae
hed44 <angim86@mail.ru>
2021-11-02 17:43:56 +0300

    catalog step 1

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                dots: true,
+                arrows: false
+              }
+            }
+        ]
+    });
+  });
